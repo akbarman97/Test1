c74650b2f5a1b652633d4396f42a4da28f628f55
User <a1techworkman@gmail.com>
2020-01-25 10:16:11 +0500

    bu dastuning daslabki arhitekturasa 25.01.2020

---
+++ app/src/main/java/com/example/test1/new_package_for_amaliyot/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package com.example.test1.new_package_for_amaliyot.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/com/example/test1/new_package_for_amaliyot/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.example.test1.new_package_for_amaliyot.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
